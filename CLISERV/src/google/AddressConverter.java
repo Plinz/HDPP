1af07f3398e4c9c3f3cf4ea74aa0161ce1f57caa
Antoine DUQUENNOY <antoine.duquennoy@etudiant.univ-lille1.fr>
2015-10-10 18:54:30 +0200

    suppr comment

--- CLISERV/src/google/AddressConverter.java
+++ CLISERV/src/google/AddressConverter.java
@@ -33,16 +33,7 @@ public class AddressConverter {
 	public GoogleResponse convertToLatLong(String fullAddress)
 			throws IOException {
 
-		/*
-		 * Create an java.net.URL object by passing the request URL in
-		 * constructor. Here you can see I am converting the fullAddress String
-		 * in UTF-8 format. You will get Exception if you don't convert your
-		 * address in UTF-8 format. Perhaps google loves UTF-8 format. :) In
-		 * parameter we also need to pass "sensor" parameter. sensor (required
-		 * parameter) — Indicates whether or not the geocoding request comes
-		 * from a device with a location sensor. This value must be either true
-		 * or false.
-		 */
+		
 		URL url = new URL(URL + "?address="
 				+ URLEncoder.encode(fullAddress, "UTF-8") + "&sensor=false");
 		// Open the Connection
@@ -60,16 +51,7 @@ public class AddressConverter {
 	public GoogleResponse convertFromLatLong(String latlongString)
 			throws IOException {
 
-		/*
-		 * Create an java.net.URL object by passing the request URL in
-		 * constructor. Here you can see I am converting the fullAddress String
-		 * in UTF-8 format. You will get Exception if you don't convert your
-		 * address in UTF-8 format. Perhaps google loves UTF-8 format. :) In
-		 * parameter we also need to pass "sensor" parameter. sensor (required
-		 * parameter) — Indicates whether or not the geocoding request comes
-		 * from a device with a location sensor. This value must be either true
-		 * or false.
-		 */
+
 		URL url = new URL(URL + "?latlng="
 				+ URLEncoder.encode(latlongString, "UTF-8") + "&sensor=false");
 		// Open the Connection
