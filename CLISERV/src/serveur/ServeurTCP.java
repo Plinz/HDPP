7a62ff93666d6fc846d76c4a9844ad1c3a4d50c5
Dowan <thomas.gosselin@etu.univ-lille1.fr>
2015-10-10 12:24:28 +0200

    ajout serv tcp

---
+++ CLISERV/src/serveur/ServeurTCP.java
@@ -0,0 +1,59 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServeurTCP {
+	private ServerSocket serveurSocket = null;
+	
+	public ServeurTCP(int port) {
+		try {
+			serveurSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public void miseEnService() {
+		Socket unClient = null;
+		
+		while (true ) {
+			try {
+				unClient = serveurSocket.accept();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		
+			realiseService(unClient);
+		}
+	}
+	
+	private void realiseService(Socket unClient) {
+		PrintWriter envoi = null;
+		BufferedReader reception = null;
+		try {
+	
+			envoi = new PrintWriter(unClient.getOutputStream(), true);
+			
+			reception = new BufferedReader(
+                    new InputStreamReader(unClient.getInputStream()));
+	
+			String message = reception.readLine();
+			System.out.println(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	
+
+	}
+	public static void main(String[] args){
+		ServeurTCP serv = new ServeurTCP( 4242);
+		serv.miseEnService();
+	}
+}
