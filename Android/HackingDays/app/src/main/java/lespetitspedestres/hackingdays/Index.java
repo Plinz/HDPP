6870a1a1898db77dc1faee54feebd1af1dcded4c
Dowan <thomas.gosselin@etu.univ-lille1.fr>
2015-10-10 15:00:49 +0200

    voila le résal a tester

--- Android/HackingDays/app/src/main/java/lespetitspedestres/hackingdays/Index.java
+++ Android/HackingDays/app/src/main/java/lespetitspedestres/hackingdays/Index.java
@@ -17,79 +17,78 @@ import java.net.UnknownHostException;
 
 public class Index extends Activity {
 
-    private Socket socket;
+	private Socket socket;
 
-    private static final int SERVERPORT = 5000;
-    private static final String SERVER_IP = "176.31.118.118";
+	private static final int SERVERPORT = 4242;
+	private static final String SERVER_IP = "176.31.118.118";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        new Thread(new ClientThread()).start();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		new Thread(new ClientThread()).start();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_index);
+	}
+	private void onPause() {
+		super.onPause();
+		socket.close();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_index, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_index, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+		// noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
 
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 
-    public void doRegister(View view){
-        Intent intentr = new Intent(Index.this,Register.class);
-        startActivity(intentr);
-    }
+	public void doRegister(View view) {
+		Intent intentr = new Intent(Index.this, Register.class);
+		startActivity(intentr);
+	}
 
-    public void doLogin(View view){
-        try {
-            String str = "coucou";
-            PrintWriter out = new PrintWriter(new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream())),
-                    true);
-            out.println(str);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Intent intentl = new Intent(Index.this,Login.class);
-        startActivity(intentl);
-    }
+	public void doLogin(View view) {
+		try {
+			String str = "coucou";
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream())), true);
+			out.println(str);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Intent intentl = new Intent(Index.this, Login.class);
+		startActivity(intentl);
+	}
 
-    class ClientThread implements Runnable {
+	class ClientThread implements Runnable {
 
-        @Override
-        public void run() {
-
-            try {
-              InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-
-            socket = new Socket(serverAddr, SERVERPORT);
-
-        } catch (UnknownHostException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-    }
-
-}}
+		@Override
+		public void run() {
+			try {
+				InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+				socket = new Socket(serverAddr, SERVERPORT);
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+}
--- Android/HackingDays/app/src/main/java/lespetitspedestres/hackingdays/Register.java
+++ Android/HackingDays/app/src/main/java/lespetitspedestres/hackingdays/Register.java
@@ -1,46 +1,129 @@
 package lespetitspedestres.hackingdays;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import org.apache.http.conn.ClientConnectionManager;
 
+import Index.ClientThread;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends Activity {
-	
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-    	Client client = new CLient(InetAddress.getAllByName("176.31.118.118"), 4242);
-    	client.send();
-    }
+	private Socket socket;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
-    }
+	private static final int SERVERPORT = 4242;
+	private static final String SERVER_IP = "176.31.118.118";
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.ll
-        int id = item.getItemId();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		new Thread(new ClientThread()).start();
+		setContentView(R.layout.activity_register);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_register, menu);
+		return true;
+	}
+
+	@Override
+	private void onPause() {
+		super.onPause();
+		socket.close();
+	}
+
+	public void doRegister(View view) {
+		try {
+			String str = "RGTR:";
+
+			TextView adresse = null;
+			adresse = (TextView) findViewById(R.id.adresse);
+
+			str += adresse.toString() + ",";
+
+			TextView ville = null;
+			ville = (TextView) findViewById(R.id.ville);
+			str += ville.toString() + ",";
+
+			TextView pays = null;
+			pays = (TextView) findViewById(R.id.pays);
+			str += pays.toString() + ",";
+
+			TextView code_postal = null;
+			code_postal = (TextView) findViewById(R.id.pays);
+			str += code_postal.toString() + "|";
+			TextView email = null;
+			email = (TextView) findViewById(R.id.pays);
+			str += email.toString() + "|";
+			TextView nom = null;
+			nom = (TextView) findViewById(R.id.pays);
+			str += nom.toString() + "|";
+			TextView prenom = null;
+			prenom = (TextView) findViewById(R.id.pays);
+			str += prenom.toString() + "|";
+			TextView age = null;
+			age = (TextView) findViewById(R.id.pays);
+			str += age.toString() + "|";
+			TextView pass = null;
+			pass = (TextView) findViewById(R.id.pays);
+			str += pass.toString();
+			Toast toast = new Toast(getApplicationContext(), str);
+			toast.show
+			
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream())), true);
+			out.println(str);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.ll
+		int id = item.getItemId();
+
+		// noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	class ClientThread implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+				socket = new Socket(serverAddr, SERVERPORT);
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 }
